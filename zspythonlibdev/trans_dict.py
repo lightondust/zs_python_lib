fedcd4b174383ad00cdd4fad086b353666f2f76c
zs mcp <choumori@choumorinoMacBook-puro.local>
2018-04-02 21:54:39 +0900

    ドキュメント

--- zspythonlibdev/trans_dict.py
+++ zspythonlibdev/trans_dict.py
@@ -1,4 +1,11 @@
-def trans_dict(dict_list, key):
+"""
+辞書を変換するための関数たち
+"""
+
+def list_to_dict(dict_list, key):
+    """
+    辞書のリストを受け取って特定のkeyの各辞書の値をkeyにして新しい辞書を作る。
+    """
     result = {}
     for item in dict_list:
         result[item[key]] = item
